0a7323e56aa56e490ec4937ff9d3baeee85cc2d3
mfcoder <florianmichel@192.168.4.132>
2014-07-18 12:40:00 +0200

    switch to java backend first functions
    switch to java backend first functions

--- app/controllers/contentView.js
+++ app/controllers/contentView.js
@@ -1,47 +1,48 @@
 "use strict";
 var mod = require('bencoding.blur');
 
-Array.prototype.removeAt = function (index) {
-	this.splice(index , 1);
+Array.prototype.removeAt = function(index) {
+	this.splice(index, 1);
 };
 
 Alloy.Globals.currentWindow = $.winHome;
 
 var moment = require("moment-with-langs");
-var args = arguments [0] || {};
+var args = arguments[0] || {};
 //var dateDataSet = new Array ();
 var dateDataSet = [];
 const LISTITEM_HEIGHT = 320;
 const BLUR_RADIUS = 1;
 
 var refreshControl = Ti.UI.createRefreshControl({
-	tintColor: 'red'
+	tintColor : 'red'
 });
 
-function scrambleWord (s) {
-	var word = s.split('') , scram = '';
+function scrambleWord(s) {
+	if(s === undefined ) return "";
+	var word = s.split(''), scram = '';
 	while (word.length) {
-		scram += word.splice(Math.floor(Math.random()*word.length), 1) [0];
+		scram += word.splice(Math.floor(Math.random()*word.length), 1)[0];
 	}
 	return scram;
 }
 
 $.listView.refreshControl = refreshControl;
 
-refreshControl.addEventListener('refreshstart' , function (e) {
+refreshControl.addEventListener('refreshstart', function(e) {
 	Ti.API.info('refreshstart');
-	loadEvents($.svLocations.views [$.svLocations.currentPage].office_id);
+	loadEvents($.svLocations.views[$.svLocations.currentPage].office_id);
 });
 
-function listView_Delete (e) {
+function listView_Delete(e) {
 	Ti.API.debug("itemindex = " + e.itemIndex);
-	for (var i = 0 ; i < $.listView.sections [0].items.length ; i++) {
-		Ti.API.debug("item: " + JSON.stringify($.listView.sections[0].items [i]));
+	for (var i = 0; i < $.listView.sections[0].items.length; i++) {
+		Ti.API.debug("item: " + JSON.stringify($.listView.sections[0].items[i]));
 	}
-	Ti.API.debug("deleting ..." + JSON.stringify(dateDataSet [e.itemIndex]));
+	Ti.API.debug("deleting ..." + JSON.stringify(dateDataSet[e.itemIndex]));
 
-	eventToDelete = Alloy.createModel('Event' , {
-		id: dateDataSet [e.itemIndex].eventId
+	eventToDelete = Alloy.createModel('Event', {
+		id : dateDataSet[e.itemIndex].eventId
 	});
 
 	eventToDelete.destroy();
@@ -49,19 +50,19 @@ function listView_Delete (e) {
 	dateDataSet.removeAt(e.itemIndex);
 }
 
-function listView_Itemclick (e) {
+function listView_Itemclick(e) {
 
-	Alloy.Globals.GoogleAnalytics.trackEvent("contentView" , "listView_Itemclick");
-	var section = $.listView.sections [e.sectionIndex];
+	Alloy.Globals.GoogleAnalytics.trackEvent("contentView", "listView_Itemclick");
+	var section = $.listView.sections[e.sectionIndex];
 
 	var item = section.getItemAt(e.itemIndex);
 
 	var dateDetailsCtrl = Alloy.Globals.Windows.getEventDetailsCtrl().init({
-		dateId: item.properties.eventId ,
-		date: item.properties.eventDate ,
-		officeId: $.svLocations.views [$.svLocations.currentPage].office_id ,
-		canteen: item.properties.canteen ,
-		lunchTag: item.properties.lunchTag
+		dateId : item.properties.eventId,
+		date : item.properties.eventDate,
+		officeId : $.svLocations.views[$.svLocations.currentPage].office_id,
+		canteen : item.properties.canteen,
+		lunchTag : item.properties.lunchTag
 	});
 	Alloy.Globals.NavigationWindow.openWindow(Alloy.Globals.Windows.getEventDetails());
 	// $.ivBackground.animate({
@@ -70,76 +71,76 @@ function listView_Itemclick (e) {
 	// });
 }
 
-function btnAddDate_Click (e) {
+function btnAddDate_Click(e) {
 
-	Alloy.Globals.GoogleAnalytics.trackEvent("contentView" , "btnAddDate_Click");
+	Alloy.Globals.GoogleAnalytics.trackEvent("contentView", "btnAddDate_Click");
 	var createDateCtrl = Alloy.createController("createDate");
 	Ti.API.debug("createDateCtrl.getView = " + createDateCtrl.getView());
 	Ti.API.debug("Global NavWindow = " + Alloy.Globals.NavigationWindow.openWindow);
 	Alloy.Globals.NavigationWindow.openWindow(createDateCtrl.getView());
 }
 
-$.svLocations.addEventListener("scrollEnd" , svLocation_scrollend);
+$.svLocations.addEventListener("scrollEnd", svLocation_scrollend);
 
 var currentPage = -1;
 
-function svLocation_scrollend (e) {
+function svLocation_scrollend(e) {
 	//get current scrollview
 	//Ti.API.debug("setting background image = " +
 	// $.svLocations.views [e.currentPage].office_id);
-	Alloy.Globals.GoogleAnalytics.trackEvent("contentView" , "svLocation_scrollend");
-	loadEvents($.svLocations.views [e.currentPage].office_id);
+	Alloy.Globals.GoogleAnalytics.trackEvent("contentView", "svLocation_scrollend");
+	loadEvents($.svLocations.views[e.currentPage].office_id);
 
 	if (currentPage != -1) {
-		$.pagingControl.children [currentPage].animate({
-			backgroundColor: "transparent" ,
-			duration: 500
+		$.pagingControl.children[currentPage].animate({
+			backgroundColor : "transparent",
+			duration : 500
 		});
 	}
 
-	$.pagingControl.children [e.currentPage].animate({
-		backgroundColor: "#F2E394" ,
-		duration: 500
+	$.pagingControl.children[e.currentPage].animate({
+		backgroundColor : "#F2E394",
+		duration : 500
 	});
 
 	currentPage = e.currentPage;
 
 }
 
-$.winHome.addEventListener("focus" , function (e) {
+$.winHome.addEventListener("focus", function(e) {
 	//$.videoPlayer.play();
 
 	Ti.API.debug("winHome:focus");
 });
 
-Ti.App.addEventListener("office_found" , function (e) {
+Ti.App.addEventListener("office_found", function(e) {
 
 	$.svLocations.removeAllChildren();
 	var views = [];
 
-	for (var i = 0 ; i < e.offices.length ; i++) {
+	for (var i = 0; i < e.offices.length; i++) {
 
-		if (e.offices [i].name) {
+		if (e.offices[i].name) {
 
 			var view = Ti.UI.createView({
-				"class": "titleCtrl" ,
-				office_id: e.offices [i].id ,
-				width: Ti.UI.SIZE ,
-				height: Ti.UI.SIZE
+				"class" : "titleCtrl",
+				office_id : e.offices[i].id,
+				width : Ti.UI.SIZE,
+				height : Ti.UI.SIZE
 			});
 
 			view.add(Ti.UI.createLabel({
-				"class": "titleLocation" ,
-				text: e.offices [i].name.toUpperCase() ,
-				width: Ti.UI.SIZE ,
-				color: "#9CBCD8"
+				"class" : "titleLocation",
+				text : e.offices[i].name.toUpperCase(),
+				width : Ti.UI.SIZE,
+				color : "#9CBCD8"
 
 			}));
 			views.push(view);
 
 			var pagingControlView = Ti.UI.createView({
-				width: Ti.Platform.displayCaps.platformWidth / e.offices.length ,
-				height: Ti.UI.FILL
+				width : Ti.Platform.displayCaps.platformWidth / e.offices.length,
+				height : Ti.UI.FILL
 			});
 			$.pagingControl.add(pagingControlView);
 		}
@@ -148,54 +149,68 @@ Ti.App.addEventListener("office_found" , function (e) {
 	$.svLocations.views = views;
 
 	svLocation_scrollend({
-		currentPage: 0
+		currentPage : 0
 	});
 
 	Alloy.Globals.loading.hide();
 });
 
-function _init () {
+function _init() {
 
 	Alloy.Globals.GoogleAnalytics.trackPageview('contentView');
 	Alloy.Globals.NavigationWindow = $.rootHome;
 	Alloy.Globals.loading.show();
 	var offices = Alloy.Collections.instance("office");
-	offices.fetch({
-		urlparams: {
-			lon: Ti.App.Properties.getObject('currentLocation').longitude ,
-			lat: Ti.App.Properties.getObject('currentLocation').latitude ,
-			d: 10
-		} ,
-		success: function (data) {
 
-			Ti.App.fireEvent('office_found' , {
-				offices: data.toJSON()
+	var client = Ti.Network.createHTTPClient({
+		// function called when the response data is available
+		onload : function(e) {
+			Ti.API.info("Received text: " + this.responseText);
+			var data = JSON.parse(this.responseText);
+			console.log(offices);
+			Ti.App.fireEvent('office_found', {
+				offices : data
 			});
-		}
-
+		},
+		// function called when an error occurs, including a timeout
+		onerror : function(e) {
+			Ti.API.debug(e.error);
+			if (_opts.error) {
+				Ti.API.debug("get office error with callback ...");
+				_opts.error(e);
+			}
+		},
+		timeout : 5000 // in milliseconds
 	});
+
+	client.open("GET", require("alloy").CFG.restapi + "office");
+	client.send();
 }
 
-Ti.App.addEventListener("updateLunchDates" , function (e) {
-    Ti.API.debug("contentView: event updateLunchDates detected, currentPage = " + currentPage);
-    
-	loadEvents($.svLocations.views [currentPage].office_id);
+Ti.App.addEventListener("updateLunchDates", function(e) {
+	Ti.API.debug("contentView: event updateLunchDates detected, currentPage = " + currentPage);
+
+	loadEvents($.svLocations.views[currentPage].office_id);
 });
 
-function loadEvents (office_id) {
+function loadEvents(office_id) {
 
 	var events = Alloy.Collections.event;
-
-	events.fetch({
-		custompath: "byOffice" ,
-		urlparams: {
-			office_id: office_id
-		} ,
-		success: function () {
-
+	
+	
+	//TODO: filter by office
+	
+	
+	var client = Ti.Network.createHTTPClient({
+		// function called when the response data is available
+		onload : function(e) {
+			Ti.API.info("Received text: " + this.responseText);
+			var events = JSON.parse(this.responseText);
+			console.log(events);
+			
 			Ti.API.debug("#lunch dates = " + events.length);
 			var dateSection = Ti.UI.createListSection({
-				headerView: Ti.UI.createView({})
+				headerView : Ti.UI.createView({})
 			});
 
 			dateDataSet = [];
@@ -204,87 +219,89 @@ function loadEvents (office_id) {
 			$.listView.visible = (events.length > 0);
 
 			var start = moment();
+			
 
-			for (var i = 0 ; i < events.length ; i++) {
-				var element = events.at(i);
-				var lunchtime = element.get("start_time");
+			for (var i = 0; i < events.length; i++) {
+				var element = events[i];
+				console.log(element);
 
-				var diff = moment(lunchtime).diff(moment() , "minutes");
+				var lunchtime = element.appointmentDate;
+
+				var diff = moment(lunchtime).diff(moment(), "minutes");
 				Ti.API.debug("diff = " + diff);
 
 				if (!_.isEmpty(element)) {
 
-					var cf = element.get("custom_fields");
 					dateDataSet.push({
-						date: {
-							text: moment(lunchtime).format("DD.MM.YYYY - HH:mm") + " Uhr"
-						} ,
-						location: {
-							text: element.get("place").name.toUpperCase()
-						} ,
-						participant1: {
-							text: (cf.participants [0] !== undefined ? (diff <= 15 ? cf.participants [0].name : scrambleWord(cf.participants [0].name)) : "unbesetzt")
-						} ,
-						participant1Image: {
-							image: (cf.participants [0] !== undefined ? (diff > 15 ? mod.createGPUBlurImageView({
-								image: cf.participants [0].photo_url ,
-								blur: {
-									type: mod.IOS_BLUR ,
-									radiusInPixels: BLUR_RADIUS
+						date : {
+							text : moment(lunchtime).format("DD.MM.YYYY - HH:mm") + " Uhr"
+						},
+						location : {
+							text : element.canteen.name.toUpperCase()
+						},
+						participant1 : {
+							text : (element.participants[0] !== undefined ? (diff <= 15 ? element.participants[0].username : scrambleWord(element.participants[0].username)) : "unbesetzt")
+						},
+						participant1Image : {
+							image : (element.participants[0] !== undefined ? (diff > 15 ? mod.createGPUBlurImageView({
+								image : element.participants[0].photo_url,
+								blur : {
+									type : mod.IOS_BLUR,
+									radiusInPixels : BLUR_RADIUS
 								}
-							}).toImage() : cf.participants [0].photo_url) : "profile.png")
-						} ,
-						participant2: {
-							text: (cf.participants [1] !== undefined ? (diff <= 15 ? cf.participants [1].name : scrambleWord(cf.participants [1].name)) : "unbesetzt")
-						} ,
-						participant2Image: {
-							image: (cf.participants [1] !== undefined ? (diff > 15 ? mod.createGPUBlurImageView({
-								image: cf.participants [1].photo_url ,
-								blur: {
-									type: mod.IOS_BLUR ,
-									radiusInPixels: BLUR_RADIUS
+							}).toImage() : element.participants[0].photo_url) : "profile.png")
+						},
+						participant2 : {
+							text : (element.participants[1] !== undefined ? (diff <= 15 ? element.participants[1].name : scrambleWord(element.participants[1].name)) : "unbesetzt")
+						},
+						participant2Image : {
+							image : (element.participants[1] !== undefined ? (diff > 15 ? mod.createGPUBlurImageView({
+								image : element.participants[1].photo_url,
+								blur : {
+									type : mod.IOS_BLUR,
+									radiusInPixels : BLUR_RADIUS
 								}
-							}).toImage() : cf.participants [0].photo_url) : "profile.png")
-						} ,
-						participant3: {
-							text: (cf.participants [2] !== undefined ? (diff <= 15 ? cf.participants [2].name : scrambleWord(cf.participants [2].name)) : "unbesetzt")
-						} ,
-						participant3Image: {
-							image: (cf.participants [2] !== undefined ? (diff > 15 ? mod.createGPUBlurImageView({
-								image: cf.participants [2].photo_url ,
-								blur: {
-									type: mod.IOS_BLUR ,
-									radiusInPixels: BLUR_RADIUS
+							}).toImage() : element.participants[0].photo_url) : "profile.png")
+						},
+						participant3 : {
+							text : (element.participants[2] !== undefined ? (diff <= 15 ? element.participants[2].name : scrambleWord(element.participants[2].name)) : "unbesetzt")
+						},
+						participant3Image : {
+							image : (element.participants[2] !== undefined ? (diff > 15 ? mod.createGPUBlurImageView({
+								image : element.participants[2].photo_url,
+								blur : {
+									type : mod.IOS_BLUR,
+									radiusInPixels : BLUR_RADIUS
 								}
-							}).toImage() : cf.participants [0].photo_url) : "profile.png")
-						} ,
-						participant4: {
-							text: (cf.participants [3] !== undefined ? (diff <= 15 ? cf.participants [3].name : scrambleWord(cf.participants [3].name)) : "unbesetzt")
-						} ,
-						participant4Image: {
-							image: (cf.participants [3] !== undefined ? (diff > 15 ? mod.createGPUBlurImageView({
-								image: cf.participants [3].photo_url ,
-								blur: {
-									type: mod.IOS_BLUR ,
-									radiusInPixels: BLUR_RADIUS
+							}).toImage() : element.participants[0].photo_url) : "profile.png")
+						},
+						participant4 : {
+							text : (element.participants[3] !== undefined ? (diff <= 15 ? element.participants[3].name : scrambleWord(element.participants[3].name)) : "unbesetzt")
+						},
+						participant4Image : {
+							image : (element.participants[3] !== undefined ? (diff > 15 ? mod.createGPUBlurImageView({
+								image : element.participants[3].photo_url,
+								blur : {
+									type : mod.IOS_BLUR,
+									radiusInPixels : BLUR_RADIUS
 								}
-							}).toImage() : cf.participants [0].photo_url) : "profile.png")
-						} ,
-						properties: {
-							backgroundColor: "transparent" ,
-							selectedBackgroundColor: "transparent" ,
-							height: LISTITEM_HEIGHT ,
-							eventId: element.get("id") ,
-							eventDate: element.get("start_time") ,
-							lunchTag: element.get("custom_fields").lunchTag ,
-							canteen: element.get("place")
+							}).toImage() : element.participants[0].photo_url) : "profile.png")
+						},
+						properties : {
+							backgroundColor : "transparent",
+							selectedBackgroundColor : "transparent",
+							height : LISTITEM_HEIGHT,
+							eventId : element.appointmentID,
+							eventDate : element.appointmentDate,
+							lunchTag : "", //element.get("custom_fields").lunchTag,
+							canteen : element.canteen.name
 						}
 					});
 				}
 			}
 
 			var sections = [];
-			$.listView.setSections( []);
+			$.listView.setSections([]);
 
 			var height = LISTITEM_HEIGHT * dateDataSet.length;
 
@@ -298,16 +315,26 @@ function loadEvents (office_id) {
 
 			refreshControl.endRefreshing();
 
-			setTimeout(function () {
+			setTimeout(function() {
 				Alloy.Globals.loading.hide();
-			} , 1000);
+			}, 1000);
 
-		} ,
-		error: function (collection , response) {
-			Ti.API.error("error " + JSON.stringify(collection));
-		}
-
+			
+			
+		},
+		// function called when an error occurs, including a timeout
+		onerror : function(e) {
+			Ti.API.debug(e.error);
+			if (_opts.error) {
+				Ti.API.debug("get office error with callback ...");
+				_opts.error(e);
+			}
+		},
+		timeout : 5000 // in milliseconds
 	});
+
+	client.open("GET", require("alloy").CFG.restapi + "event");
+	client.send();
 
 }
 
--- app/models/user.js
+++ app/models/user.js
@@ -1,56 +1,55 @@
 exports.definition = {
-	config: {
-		"URL": require("alloy").CFG.restapi + "user" ,
-		adapter: {
-			type: "restapi" ,
-			"idAttribute": "id" ,
-			collection_name: "offices" ,
-			"debug": 1,
-		} ,
-		headers: {
-			"_session_id": function () {
+	config : {
+		"URL" : require("alloy").CFG.restapi,
+		adapter : {
+			type : "restapi",
+			"idAttribute" : "id",
+			collection_name : "offices",
+			"debug" : 1,
+		},
+		headers : {
+			"_session_id" : function() {
 				return Ti.App.Properties.getString("acs.sessionId");
 			}
-
 		}
-	} ,
+	},
 
-	extendModel: function (Model) {
+	extendModel : function(Model) {
 
 		var that = this;
-		function login (_login , _password , _opts) {
-			var xhr = require("xhr");
-			xhr.do({
+		function login(_login, _password, _opts) {
 
-				type: "POST" ,
-				url: that.config.URL + "/login" ,
-				data: {
-					username: _login ,
-					password: _password
-				}
-
-			} , function (e) {
-				Ti.API.debug("xhr login result = " + JSON.stringify(e));
-				if (e.success) {
-
-					Ti.App.Properties.setString("acs.sessionId" , e.responseJSON.sessionId);
-					Ti.App.Properties.setString("username" , _login);
+			var client = Ti.Network.createHTTPClient({
+				// function called when the response data is available
+				onload : function(e) {
+					console.log(JSON.stringify(e));
+					Ti.API.info("Received text: " + this.responseText);
+					//Ti.App.Properties.setString("acs.sessionId", e.responseJSON.sessionId);
+					Ti.App.Properties.setString("username", _login);
 
 					if (_opts.success) {
 						Ti.API.debug("login user model success with callback ...");
 						_opts.success(e);
 					}
-				}
-				else {
+				},
+				// function called when an error occurs, including a timeout
+				onerror : function(e) {
+					Ti.API.debug(e.error);
 					if (_opts.error) {
 						Ti.API.debug("login user model error with callback ...");
 						_opts.error(e);
 					}
-				}
+				},
+				timeout : 5000 // in milliseconds
 			});
+
+			client.open("POST", require("alloy").CFG.restapi + "mam-backend-0.0.1.BUILD-SNAPSHOT-dev/j_spring_security_check?j_username="+_login+"&j_password="+_password);
+			client.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+			client.send();
+
 		}
 
-		function logout (_callback) {
+		function logout(_callback) {
 			Ti.App.Properties.removeProperty("acs.sessionId");
 			Ti.App.Properties.removeProperty("username");
 			Ti.App.Properties.removeProperty("password");
@@ -60,34 +59,33 @@ exports.definition = {
 			}
 		}
 
-		function register (_login , _password , _first_name , _last_name , _img , _opts) {
+		function register(_login, _password, _first_name, _last_name, _img, _opts) {
 			var xhr = require("xhr");
 			var img = Ti.Utils.base64encode(_img).toString();
 
 			xhr.do({
 
-				type: "POST" ,
-				url: that.config.URL + "/register" ,
-				data: {
-					username: _login ,
-					email: _login ,
-					password: _password ,
-					first_name: _first_name ,
-					last_name: _last_name ,
-					photo: img
+				type : "POST",
+				url : that.config.URL + "/register",
+				data : {
+					username : _login,
+					email : _login,
+					password : _password,
+					first_name : _first_name,
+					last_name : _last_name,
+					photo : img
 				}
 				// ,headers: {
 				// "Content-Type": "multipart/form-data"
 				// }
 
-			} , function (data) {
+			}, function(data) {
 				Ti.API.debug(JSON.stringify(data));
 				if (data.success) {
 					Ti.API.debug("acs.sessionId = " + data.responseJSON.sessionId);
-					Ti.App.Properties.setString("acs.sessionId" , data.responseJSON.sessionId);
-					Ti.App.Properties.setString("username" , _login);
-				}
-				else {
+					Ti.App.Properties.setString("acs.sessionId", data.responseJSON.sessionId);
+					Ti.App.Properties.setString("username", _login);
+				} else {
 
 				}
 				if (_opts.success) {
@@ -97,28 +95,27 @@ exports.definition = {
 			});
 		}
 
-		function saveFeedback (_login , _content , _rating , _opts) {
+		function saveFeedback(_login, _content, _rating, _opts) {
 			Ti.API.debug(_login);
 			var xhr = require("xhr");
 
 			xhr.do({
 
-				type: "POST" ,
-				url: that.config.URL + "/saveFeedback" ,
-				data: {
-					username: _login ,
-					content: _content ,
-					rating: _rating
+				type : "POST",
+				url : that.config.URL + "/saveFeedback",
+				data : {
+					username : _login,
+					content : _content,
+					rating : _rating
 				}
 
-			} , function (data) {
+			}, function(data) {
 				Ti.API.debug(JSON.stringify(data));
 				if (data.success) {
 					Ti.API.debug("acs.sessionId = " + data.responseJSON.sessionId);
-					Ti.App.Properties.setString("acs.sessionId" , data.responseJSON.sessionId);
-					Ti.App.Properties.setString("username" , _login);
-				}
-				else {
+					Ti.App.Properties.setString("acs.sessionId", data.responseJSON.sessionId);
+					Ti.App.Properties.setString("username", _login);
+				} else {
 
 				}
 				if (_opts.success) {
@@ -129,23 +126,22 @@ exports.definition = {
 		}
 
 
-		_.extend(Model.prototype , {
-			login: login ,
-			logout: logout ,
-			register: register ,
-			saveFeedback: saveFeedback
+		_.extend(Model.prototype, {
+			login : login,
+			logout : logout,
+			register : register,
+			saveFeedback : saveFeedback
 		});
 		// end extend
 
 		return Model;
-	} ,
-	extendCollection: function (Collection) {
-		_.extend(Collection.prototype , {
+	},
+	extendCollection : function(Collection) {
+		_.extend(Collection.prototype, {
 
 		});
 		// end extend
 
 		return Collection;
 	}
-
 };
